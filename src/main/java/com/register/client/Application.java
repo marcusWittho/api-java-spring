8e3e146a680b7b071badd189060b97b99bd1a74d
marcusWittho <mcswitthoeft@gmail.com>
2022-07-22 02:17:33 -0300

    feat: Iniciando a aplicação

---
+++ src/main/java/com/register/client/Application.java
@@ -0,0 +1,15 @@
+package com.register.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Classe principal.
+ */
+@SpringBootApplication
+public class Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Application.class, args);
+  }
+}
