7727443051bdc1eb1663e0c27ca2533cbed5e5fa
marcusWittho <mcswitthoeft@gmail.com>
2022-07-22 23:04:44 -0300

    feat: Implementação do CRUD

--- README.md
+++ README.md
@@ -1,9 +1,19 @@
-# Projeto Java desenvolvido para aprender sobre o deploy utilizando o Heroku.
+# Projeto Java desenvolvido para aprender mais sobre Spring-Boot e deploy utilizando o Heroku.
+
+CRUD utilizando spring-boot. (repo em desenvolvimento)
 
   - Endereços de acesso da api:
-    - Adiciona automaticamente cinco clientes:
-	[Adiciona clientes para teste](https://api-java-spring-wittho.herokuapp.com/save)
+    - Adiciona novos clientes:
+	[Endpoint para adicionar novos clientes(https://api-java-spring-wittho.herokuapp.com/register)
+
     - Retorna todos os clientes adicionados:
-	[Retorna os clientes adicionados](https://api-java-spring-wittho.herokuapp.com/clients)
+	[Endpoint para retornar os clientes cadastrados(https://api-java-spring-wittho.herokuapp.com/register)
+
     - Retorna o cliente de acordo com o id informado:
-	[Retorna o cliente pelo ID](https://api-java-spring-wittho.herokuapp.com/client/4)
+	[Endpoint para retornar cliente pelo ID](https://api-java-spring-wittho.herokuapp.com/register/1)
+
+    - Remove cliente de acordo com o id informado:
+	[Endpoint para remover cliente pelo ID](https://api-java-spring-wittho.herokuapp.com/register/1))
+
+    - Atualiza o cliente de acordo com o id informado:
+	[Endpoint para atualizar cliente pelo ID](https://api-java-spring-wittho.herokuapp.com/register/1))
--- src/main/java/com/register/client/controller/ClientController.java
+++ src/main/java/com/register/client/controller/ClientController.java
@@ -2,42 +2,96 @@ package com.register.client.controller;
 
 import com.register.client.model.Client;
 import com.register.client.repository.ClientRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Classe Controller.
  */
 @RestController
+@RequestMapping(path = "/register")
 public class ClientController {
   
-  @Autowired
   private ClientRepository repository;
+  
+  public ClientController(ClientRepository repository) {
+    super();
+    this.repository = repository;
+  }
 
   /**
    * Método responsável por adicionar um novo cliente. 
    */
-  @GetMapping("/save")
-  public String addClient() {
+  @PostMapping
+  public ResponseEntity<Client> addClient(@RequestBody Client client) {
     
-    repository.save(new Client("Cliente 1", 10, "cliente1@email.com"));
-    repository.save(new Client("Cliente 2", 20, "cliente2@email.com"));
-    repository.save(new Client("Cliente 3", 30, "cliente3@email.com"));
-    repository.save(new Client("Cliente 4", 40, "cliente4@email.com"));
-    repository.save(new Client("Cliente 5", 50, "cliente5@email.com"));
-    
-    return "Client added.";
+    return ResponseEntity.status(200).body(repository.save(client));
   }
   
-  @GetMapping("/clients")
-  public Iterable<Client> clients() {
-    return repository.findAll();
+  /**
+   * Método responsável por retornar uma lista com todos os clientes cadastrados. 
+   */
+  @GetMapping
+  public ResponseEntity<Iterable<Client>> clients() {
+
+    Iterable<Client> clients = new ArrayList<>();
+    clients = repository.findAll();
+    
+    return ResponseEntity.status(201).body(clients);
   }
   
-  @GetMapping("/client/{id}")
-  public Client client(@PathVariable long id) {
-    return repository.findById(id).orElse(new Client());
+  /**
+   * Método responsável por retornar o cliente com o ID informado por parâmetro. 
+   */
+  @GetMapping("/{id}")
+  public ResponseEntity<Optional<Client>> getClient(@PathVariable long id) {
+
+    Optional<Client> client;
+    
+    try {
+      client = repository.findById(id);
+      return ResponseEntity.status(201).body(client);
+    } catch (NoSuchElementException err) {
+      return ResponseEntity.status(404).build();
+    }
+  }
+  
+  /**
+   * Método responsável por remover o cliente com o ID informado por parâmetro. 
+   */
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Optional<Client>> removeClient(@PathVariable Long id) {
+    try {
+      repository.deleteById(id);
+      return ResponseEntity.status(201).build();
+    } catch (NoSuchElementException err) {
+      return ResponseEntity.status(404).build();
+    }
+  }
+  
+  /**
+   * Método responsável por atualizar determinado cliente.
+   */
+  @PutMapping("/{id}")
+  public ResponseEntity<Client> updateClient(@PathVariable Long id, @RequestBody Client client) {
+    
+    return repository.findById(id)
+        .map(foundClient -> {
+          foundClient.setName(client.getName());
+          foundClient.setAge(client.getAge());
+          foundClient.setEmail(client.getEmail());
+          Client clientUpdated = repository.save(foundClient);
+          return ResponseEntity.ok().body(clientUpdated);
+        }).orElse(ResponseEntity.notFound().build());
   }
 }
